33c11da47d51501328d286b341c50dfc60d1a399
hjhawkinsiv <harry@createlab.org>
2021-02-09 23:37:43 -0800

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+setup(
     name='jacket',
     version='0.1.0',
-    packages=setuptools.find_packages(),
-    url='https://github.com/CMU-CREATE-Lab/unearthtime',
+    packages=find_packages(),
+    url='https://github.com/junk-io/jacket',
     author='junki',
     description='A Selenium-based tool for wrapping and interacting with JavaScript objects',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
     data_files=[('', ['LICENSE'])],
     install_requires=[
         'selenium'
