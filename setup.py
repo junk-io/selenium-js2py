c85137c8f0d24f03a7320a777759fac58258e4bf
hjhawkinsiv <harry@createlab.org>
2021-02-13 12:18:54 -0800

    0.3.0

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
-    name='selenium_js2py',
-    version='0.2.0',
+    name="selenium_js2py",
+    version="0.3.0",
     packages=find_packages(),
-    url='https://github.com/junk-io/selenium-js2py',
-    author='junki',
-    description='A Selenium-based tool for wrapping and interacting with JavaScript objects',
+    url="https://github.com/junk-io/selenium-js2py",
+    author="junki",
+    description="A Selenium-based tool for wrapping and interacting with JavaScript objects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    data_files=[('', ['LICENSE'])],
+    data_files=[("", ["LICENSE", "README.md"])],
     install_requires=[
-        'selenium'
+        "selenium"
     ]
 )
